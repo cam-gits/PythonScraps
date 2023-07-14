70cfadb872efaf8c8f41689dc211cb419aeb4106
cam-gits <136761134+cam-gits@users.noreply.github.com>
2023-07-14 19:13:24 +0100

    Add files via upload

--- tasklist.py
+++ tasklist.py
@@ -14,12 +14,12 @@ print("Current list location: {} Update in (S)ettings".format(taskList))
 
 while command:
 
-    #input command, no validation, validation through if/else in body
+    # input command, no validation, validation through if/else in body
     command = input("Select an option to begin: ").upper()
     items = 0
 
     
-    #loop for reading line items on the list and printing to console, with counter to verify completion
+    # loop for reading line items on the list and printing to console, with counter to verify completion
     if command == 'R':
         print("Fetching list...")
         if os.path.isfile(taskList):
@@ -34,7 +34,7 @@ while command:
         else:
             print("You have no tasklist at {}".format(taskList))
             
-    #The below elif deals with writing an item to the list as a new line, if no file exists one will be created
+    # the below deals with writing an item to the list as a new line, if no file exists one will be created
     elif command == 'A' or command == 'W':
         try:
             if os.path.isfile(taskList):
@@ -48,7 +48,7 @@ while command:
         except:
             print('The filepath {} is not valid, please update it in settings (S)'.format(taskList))
 
-    # deletes tasks containing string 
+    # deletes tasks containing user inputted string 
     elif command == 'D':
         print('All tasks containing the specified text will be deleted')
         dropLine = input('Specify text: ')
@@ -61,24 +61,26 @@ while command:
                     items = items + 1
         print('Items containing "{}" have been removed, if any ever existed...\n{} list items remaining'.format(dropLine, items))
 
-    #Decision path to deleting the whole file
+    # decision path to deleting the whole file
     elif command == 'DL':
         print("You have chosen to DELETE THE ENTIRE LIST, are you sure? (Y/N)")
         print("This cannot be undone")
         delList = input().upper()
         try:
-            if delList == 'Y':
+            if delList == 'Y' and os.path.isfile(taskList):
                 os.remove(taskList)
                 print('The task list has been deleted')
             elif delList == 'N':
                 print('File retained, returning to main')
+            elif os.path.isfile(taskList) != True:
+                print('There is no current tasklist to remove')
             else:
                 print("I don't recognise this command.")
                 print('File retained, returning to main')
         except:
             print('The filepath {} is not valid, please update it in settings (S)'.format(taskList))
 
-    #Option to change file location
+    # option to change file location
     elif command == 'S':
         changePath = input('Would you like to change the location of your tasklist? (Y/N): ').upper()
         if changePath == 'Y':
@@ -89,12 +91,12 @@ while command:
         else:
             print('Invalid Option. Returning to Main')
 
-    #exit clause
+    # exit programme
     elif command == 'E':
         print("Goodbye")
         break
 
-    #Unrecognised command
+    # unrecognised command
     else:
         print("\nThis is not a valid option.\n")
         print("Commands for this programme are: \nRead List (R)\nAdd Item (A or W)\nDelete Item (D)\nDelete List (DL)\nSettings (S)\nExit App (E)\n")
