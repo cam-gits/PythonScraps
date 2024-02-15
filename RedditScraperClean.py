67e4153fd3e8265ead04c3ff9319972be63c5d4d
cam-gits <136761134+cam-gits@users.noreply.github.com>
2024-02-15 12:14:00 +0000

    Add files via upload
    A little scraper to pull the most recent posts from r/develeire into a CSV for analysis, I built this on noticing the tone of the subreddit change, and wanted to see if it could be used to track industry sentiment.

---
+++ RedditScraperClean.py
@@ -0,0 +1,30 @@
+import praw
+import csv
+from datetime import datetime
+
+# USE reddit.com/prefs/apps 
+reddit_client_id = 'ID'
+reddit_client_secret = 'SECRET'
+reddit_user_agent = 'AGENT'  
+
+# Create a Reddit instance
+reddit = praw.Reddit(
+    client_id=reddit_client_id,
+    client_secret=reddit_client_secret,
+    user_agent=reddit_user_agent
+)
+
+#def scrape to CSV
+def scrape_reddit(subreddit_name, num_posts=10):
+    subreddit = reddit.subreddit(subreddit_name)
+    new_posts = subreddit.new(limit=num_posts)
+
+    for post in new_posts:
+        with open('reddit.csv', 'a', newline='') as csvfile:
+            created = datetime.utcfromtimestamp(post.created)
+            fieldnames = ['Title', 'Date','URL','Text','Score','Number of Comments']
+            postdump = csv.writer(csvfile, delimiter=',')
+            postdump.writerow([post.title,created,post.url,post.selftext[:150],post.score,post.num_comments])
+
+scrape_reddit('DevelEire', num_posts=50)
+print('Done')
